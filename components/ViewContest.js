d00fd8d9be61b2af885d0b288944b67701ed7f5a
Jay <digvijaykadam61998@gmail.com>
2022-01-25 20:31:01 +0530

    solved error

---
+++ components/ViewContest.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+
+const ViewContest = () => {
+  return (
+    <View>
+       
+    </View>
+  );
+};
+
+export default ViewContest;
+
+const styles = StyleSheet.create({});
